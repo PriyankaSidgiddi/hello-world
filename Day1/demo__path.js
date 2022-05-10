5a6a4960416c3a2d33936ce80934c4c33df44cd7
PriyankaSidgiddi <priyanka.sidgiddi@freightdeck.com>
2022-05-10 13:16:35 +0530

    firstCommit

---
+++ Day1/demo__path.js
@@ -0,0 +1,5 @@
+var path = require('path');
+var filename = path.basename('/Priyanka/Day 1/demo_path.js');
+var filename1 = path.delimiter;
+var filename2 = path.dirname('/Priyanka/Day 1/demo_path.js');
+console.log("Hi",filename2);
